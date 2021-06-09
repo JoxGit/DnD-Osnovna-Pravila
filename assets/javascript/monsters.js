8323e33ea18a4eb2e6451612c2cfe1127a9bca93
JoxTorn <43649390+JoxTorn@users.noreply.github.com>
2021-06-09 23:30:25 +0200

    Izmjene

--- assets/javascript/monsters.js
+++ assets/javascript/monsters.js
@@ -1,7 +1,119 @@
 (function () {
 
-    ajax('GET', 'https://joxgit.github.io/DnD-Osnovna-Pravila/dnd.json', [], '', parseDndJson);
+    ajax('GET', 'https://joxgit.github.io/DnD-Osnovna-Pravila/monsters.json', [], '', parseMonstersJson);
 
+    function parseMonstersJson(response) {
+//        try {
+            let div = document.getElementById('container');
+            let obj = JSON.parse(response.responseText);
+            let i = 0;
+            let template = document.getElementById('template-small').content;
+            let cardsOnTemplate = 4;
+            let cardsOnTemplateRemaining = 0;
+            let templateInstance;
+
+            for(let monster of obj){
+
+                //checking if i have remaining cards on template
+                if(cardsOnTemplateRemaining == 0){
+                    //create new template instance
+                    templateInstance = document.importNode(template, true);
+                    cardsOnTemplateRemaining = cardsOnTemplate;
+                }
+
+                //spending one of cards on template
+                cardsOnTemplateRemaining--;
+
+                let currentCardOnTemplate = cardsOnTemplate - cardsOnTemplateRemaining
+
+                //populating back of page
+                templateInstance.querySelector(`.page-back .card-${currentCardOnTemplate} .name`).innerHTML = monster.name;
+                templateInstance.querySelector(`.page-back .card-${currentCardOnTemplate} .picture`).src = monster.image;
+
+                //populating front of page
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .name`).innerHTML = monster.name;
+                //info
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .info`).innerHTML = `${monster.size} ${monster.type}, ${monster.alignament}`;
+                //attributes
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .ac`).innerHTML = `${monster.attributes.ac[0]} (${monster.attributes.ac[1]})`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .hp`).innerHTML = `${monster.attributes.hp[0]} (${monster.attributes.hp[1]})`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .speed`).innerHTML = `${monster.attributes.speed.value} ${monster.attributes.speed.extra}`;
+                //abbilities
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .str`).innerHTML = monster.abilities.str[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .dex`).innerHTML = monster.abilities.dex[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .con`).innerHTML = monster.abilities.con[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .int`).innerHTML = monster.abilities.int[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .wis`).innerHTML = monster.abilities.wis[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .cha`).innerHTML = monster.abilities.cha[1];
+                //properties
+                if(monster.properties){
+                    if(monster.properties.saving){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .saving`).innerHTML = monster.properties.saving.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .saving`).parentNode.classList.remove('hide')
+                    }
+                    if(monster.properties.skills){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .skills`).innerHTML = monster.properties.skills.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .skills`).parentNode.classList.remove('hide')
+                    }
+                    
+                    if(monster.properties.immunities && monster.properties.immunities.damage){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .immunities-damage`).innerHTML = monster.properties.immunities.damage.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .immunities-damage`).parentNode.classList.remove('hide')
+                    }
+                    if(monster.properties.immunities && monster.properties.immunities.condition){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .immunities-condition`).innerHTML = monster.properties.immunities.condition.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .immunities-condition`).parentNode.classList.remove('hide')
+                    }
+                    if(monster.properties.resistances){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .resistances`).innerHTML = monster.properties.resistances.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .resistances`).parentNode.classList.remove('hide')
+                    }
+                    if(monster.properties.vulnerabilities){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .vulnerabilities`).innerHTML = monster.properties.vulnerabilities.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .vulnerabilities`).parentNode.classList.remove('hide')
+                    }
+
+                    if(monster.properties.senses){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .senses`).innerHTML = monster.properties.senses.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .senses`).parentNode.classList.remove('hide')
+                    }
+                    if(monster.properties.languages){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .languages`).innerHTML = monster.properties.languages.join(', ');
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .languages`).parentNode.classList.remove('hide')
+                    }
+
+                    if(monster.properties.cr){
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .cr`).innerHTML = `${monster.properties.cr[0]} (${monster.properties.cr[1]})`;
+                        templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .cr`).parentNode.classList.remove('hide')
+                    }
+                }
+                //source
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .source`).innerHTML = monster.source.join(' ');
+
+                if(cardsOnTemplateRemaining == 0){
+                    //add page to document
+                    div.appendChild(templateInstance);
+                }
+
+                i++;
+                if (i > 25) {
+                    break;
+                }
+            };
+
+            //add remaining (not full) pages to document
+            if(cardsOnTemplateRemaining > 0){
+                //add page to document
+                div.appendChild(templateInstance);
+            }
+/*
+        } catch (error) {
+            console.log(error);
+        }
+*/
+    }
+
+    /*
     function parseDndJson(response) {
         try {
             let div = document.getElementById('container');
@@ -41,7 +153,7 @@
             console.log(error);
         }
     }
-
+    */
 
     function ajax(method, url, headers, data, callback, caller) {
         var xhttp = new XMLHttpRequest();
@@ -57,7 +169,7 @@
         xhttp.open(method, url, true);
 
         for (header of headers) {
-            /*xhttp.setRequestHeader("Content-type", "application/json");*/
+            s
             xhttp.setRequestHeader(header.key, header.value);
         }
 
