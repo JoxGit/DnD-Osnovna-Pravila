43582b564870b452555373a3d3ec3e16d74471fe
JoxTorn <43649390+JoxTorn@users.noreply.github.com>
2021-06-10 14:46:10 +0200

    izmjene

--- assets/javascript/monsters.js
+++ assets/javascript/monsters.js
@@ -1,30 +1,91 @@
 (function () {
 
-    ajax('GET', 'https://joxgit.github.io/DnD-Osnovna-Pravila/monsters.json', [], '', parseMonstersJson);
+    var monsters = [];
 
+    window.onload = function(){
+        document.getElementById('btnLoad').addEventListener('click', load);
+        document.getElementById('btnGenerate').addEventListener('click', generate);
+        document.getElementById('btnReset').addEventListener('click', reset);
+
+        load();
+    }
+
+    function reset(){
+        document.getElementById('container').innerHTML = '';
+    }
+
+    function load(){
+        document.getElementById('fieldMessage').innerHTML = ''
+        ajax('GET', 'https://raw.githubusercontent.com/JoxGit/DnD-Osnovna-Pravila/main/monsters.json', [], '', parseMonstersJson);
+    }
+
+    function generate(){
+        prepareForPrint();
+    }
+    
     function parseMonstersJson(response) {
-//        try {
+        try {
+
+            monsters = JSON.parse(response.responseText);
+            
+            document.getElementById('fieldMessage').innerHTML = 'Load Compelte'
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function prepareForPrint() {
+        try {
             let div = document.getElementById('container');
-            let obj = JSON.parse(response.responseText);
             let i = 0;
-            let template = document.getElementById('template-big').content;
+            let templateBig = document.getElementById('template-big').content;
+            let templateSmall = document.getElementById('template-small').content;
             let templateTrait = document.getElementById('template-trait').content;
-            let cardsOnTemplate = 2;
-            let cardsOnTemplateRemaining = 0;
-            let templateInstance;
+            let cardsOnTemplateBig = 2;
+            let cardsOnTemplateSmall = 4;
+            let cardsOnTemplateRemainingBig = 0;
+            let cardsOnTemplateRemainingSmall = 0;
+            let templateInstanceBig;
+            let templateInstanceSmall;
 
-            for(let monster of obj){
+            for(let monster of monsters){
+                let templateInstance;
+                let currentCardOnTemplate;
 
-                //checking if i have remaining cards on template
-                if(cardsOnTemplateRemaining == 0){
-                    //create new template instance
-                    templateInstance = document.importNode(template, true);
-                    cardsOnTemplateRemaining = cardsOnTemplate;
+                if(monster.card == 'Large'){
+                    //This monster should go to small card becausecause of description length
+                    
+                    //checking if i have remaining cards on template
+                    if (cardsOnTemplateRemainingBig == 0) {
+                        //create new template instance
+                        templateInstanceBig = document.importNode(templateBig, true);
+                        cardsOnTemplateRemainingBig = cardsOnTemplateBig;
+                    }
+
+                    //spending one of cards on template
+                    cardsOnTemplateRemainingBig--;
+                    currentCardOnTemplate = cardsOnTemplateBig - cardsOnTemplateRemainingBig
+
+                    //assigning to variable that is used no matter of size
+                    templateInstance = templateInstanceBig;
                 }
+                else{
+                    //This monster should go to small card becausecause of description length
+                    
+                    //checking if i have remaining cards on template
+                    if (cardsOnTemplateRemainingSmall == 0) {
+                        //create new template instance
+                        templateInstanceSmall = document.importNode(templateSmall, true);
+                        cardsOnTemplateRemainingSmall = cardsOnTemplateSmall;
+                    }
 
-                //spending one of cards on template
-                cardsOnTemplateRemaining--;
-                let currentCardOnTemplate = cardsOnTemplate - cardsOnTemplateRemaining
+                    //spending one of cards on template
+                    cardsOnTemplateRemainingSmall--;
+                    currentCardOnTemplate = cardsOnTemplateSmall - cardsOnTemplateRemainingSmall
+
+                    //assigning to variable that is used no matter of size
+                    templateInstance = templateInstanceSmall;
+                }
 
                 //populating back of page
                 templateInstance.querySelector(`.page-back .card-${currentCardOnTemplate} .name`).innerHTML = monster.name;
@@ -37,14 +98,14 @@
                 //attributes
                 templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .ac`).innerHTML = `${monster.attributes.ac[0]} (${monster.attributes.ac[1]})`;
                 templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .hp`).innerHTML = `${monster.attributes.hp[0]} (${monster.attributes.hp[1]})`;
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .speed`).innerHTML = `${monster.attributes.speed.value} ${monster.attributes.speed.extra}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .speed`).innerHTML = `${monster.attributes.speed.value}, ${monster.attributes.speed.extra.map(el => el.join(' ')).join(', ')}`;
                 //abbilities
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .str`).innerHTML = monster.abilities.str[1];
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .dex`).innerHTML = monster.abilities.dex[1];
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .con`).innerHTML = monster.abilities.con[1];
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .int`).innerHTML = monster.abilities.int[1];
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .wis`).innerHTML = monster.abilities.wis[1];
-                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .cha`).innerHTML = monster.abilities.cha[1];
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .str`).innerHTML = `${monster.abilities.str[1] >= 0 ? '+' : ''}${monster.abilities.str[1]}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .dex`).innerHTML = `${monster.abilities.dex[1] >= 0 ? '+' : ''}${monster.abilities.dex[1]}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .con`).innerHTML = `${monster.abilities.con[1] >= 0 ? '+' : ''}${monster.abilities.con[1]}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .int`).innerHTML = `${monster.abilities.int[1] >= 0 ? '+' : ''}${monster.abilities.int[1]}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .wis`).innerHTML = `${monster.abilities.wis[1] >= 0 ? '+' : ''}${monster.abilities.wis[1]}`;
+                templateInstance.querySelector(`.page-front .card-${currentCardOnTemplate} .cha`).innerHTML = `${monster.abilities.cha[1] >= 0 ? '+' : ''}${monster.abilities.cha[1]}`;
                 //properties
                 if(monster.properties){
                     if(monster.properties.saving){
@@ -101,28 +162,41 @@
                     });
                 }
                 
-
-                if(cardsOnTemplateRemaining == 0){
-                    //add page to document
-                    div.appendChild(templateInstance);
+                if(monster.card == 'Large'){
+                    if(cardsOnTemplateRemainingBig == 0){
+                        //add page to document
+                        div.appendChild(templateInstanceBig);
+                    }
                 }
-
+                else{
+                    if(cardsOnTemplateRemainingSmall == 0){
+                        //add page to document
+                        div.appendChild(templateInstanceSmall);
+                    }
+                }
+                /*
                 i++;
-                if (i > 6) {
+                if (i > 16) {
                     break;
                 }
+                */
             };
 
             //add remaining (not full) pages to document
-            if(cardsOnTemplateRemaining > 0){
+            if(cardsOnTemplateRemainingBig > 0){
                 //add page to document
-                div.appendChild(templateInstance);
+                div.appendChild(templateInstanceBig);
             }
-/*
+
+            if(cardsOnTemplateRemainingSmall > 0){
+                //add page to document
+                div.appendChild(templateInstanceSmall);
+            }
+
         } catch (error) {
             console.log(error);
         }
-*/
+
     }
 
     /*
